e1ef64b9056ee872168d635c8ff5acbe99daca38
HauDoThanh <trangnguyenchuoi@gmail.com>
2017-11-25 20:40:28 +0700

    Add files via upload

--- app/src/main/java/picktokick/devfest/picktokick/fragment/Fragment_ThongTin.java
+++ app/src/main/java/picktokick/devfest/picktokick/fragment/Fragment_ThongTin.java
@@ -3,7 +3,6 @@ package picktokick.devfest.picktokick.fragment;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -19,11 +18,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.facebook.AccessToken;
-import com.facebook.GraphRequest;
-import com.facebook.GraphResponse;
-import com.facebook.HttpMethod;
-import com.facebook.login.LoginManager;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -31,7 +25,7 @@ import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import picktokick.devfest.picktokick.R;
-import picktokick.devfest.picktokick.activitys.LoginActivity;
+import picktokick.devfest.picktokick.activitys.ShowListAddFriend;
 import picktokick.devfest.picktokick.adapter.ProfileAdapter;
 import picktokick.devfest.picktokick.objects.Constanttt;
 import picktokick.devfest.picktokick.objects.Friend;
@@ -97,18 +91,7 @@ public class Fragment_ThongTin extends Fragment implements AdapterView.OnItemCli
         name = (TextView) view.findViewById(R.id.txtName);
         anhDaiDien = (CircleImageView) view.findViewById(R.id.imgAnhDaiDien);
         btnLogout = (Button) view.findViewById(R.id.btnLogOut);
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SharedPreferences ref =getContext().getSharedPreferences(Constanttt.SHARE_REF_LOGIN,Context.MODE_PRIVATE);
-                SharedPreferences.Editor edit = ref.edit();
-                edit.clear();
-                edit.commit();
-                disconnectFromFacebook();
-                getActivity().startActivity(new Intent(getContext(), LoginActivity.class));
-                getActivity().finish();
-            }
-        });
+
         listProfile = new ArrayList<>();
         listProfile.add("Thêm bạn bè");
         listProfile.add("Lời mời kết bạn");
@@ -138,80 +121,10 @@ public class Fragment_ThongTin extends Fragment implements AdapterView.OnItemCli
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
         switch (i) {
             case 0:
-                final Dialog dialog0 = new Dialog(getContext());
-                dialog0.setTitle("Thêm bạn bè");
-                dialog0.setContentView(R.layout.dialog_add_friend);
-                dialog0.show();
-                //anh xa trong dialog add friend
-                edtInputAddFr = (EditText) dialog0.findViewById(R.id.edtInputAddFr);
-                Button btnXemThu, btnok, btncancel;
-                btncancel = (Button) dialog0.findViewById(R.id.btnCancelAddFr);
-                btnok = (Button) dialog0.findViewById(R.id.btnOkAddFr);
-                final TextView txttenXemThu = (TextView) dialog0.findViewById(R.id.txtXemThuName);
-                final ImageView imgADD = (ImageView) dialog0.findViewById(R.id.imgXemThu);
-                btnXemThu = (Button) dialog0.findViewById(R.id.btnXemAvata);
-
-
-                //btn xem thu avt nguoi ban
-                btnXemThu.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-
-                        if (edtInputAddFr.getText().toString().equals("")) {
-                            Toast.makeText(getContext(), "Vui lòng nhập thông tin cần thiết để tìm người bạn!", Toast.LENGTH_SHORT).show();
-                        } else {
-                           /* String a[] = getUserByID(edtInputAddFr.getText().toString());
-                            // Log.e(Constanttt.TAG_APP,"xxxx "+a[1]);
-                            txttenXemThu.setText(a[0]);
-                            Glide.with(getContext())
-                                    .load(a[1])
-                                    .into(imgADD);*/
-                            //load anh bang glide
-                            if (boolCLickXemTHu) {
-                                imgADD.setVisibility(View.INVISIBLE);
-
-                                boolCLickXemTHu = false;
-                            } else {
-                                imgADD.setVisibility(View.GONE);
-                                boolCLickXemTHu = true;
-                            }
-                        }
-                    }
-                });
-                //button cancel ko ket ban
-                btncancel.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        dialog0.dismiss();
-                    }
-                });
-                //button click dong y ket ban
-                btnok.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        String myID = getContext()
-                                .getSharedPreferences(Constanttt.SHARE_REF_LOGIN, Context.MODE_PRIVATE).getString(Constanttt.LOGIN_ID, null);
-                        String link = getContext()
-                                .getSharedPreferences(Constanttt.SHARE_REF_LOGIN, Context.MODE_PRIVATE).getString(Constanttt.LOGIN_LINK_IMG, null);
-                        String name = getContext()
-                                .getSharedPreferences(Constanttt.SHARE_REF_LOGIN, Context.MODE_PRIVATE).getString(Constanttt.LOGIN_NAME, null);
-                        String x = getContext()
-                                .getSharedPreferences(Constanttt.SHARE_REF_LOGIN, Context.MODE_PRIVATE).getString(Constanttt.LOGIN_LATITUDE, null);
-                        String y = getContext()
-                                .getSharedPreferences(Constanttt.SHARE_REF_LOGIN, Context.MODE_PRIVATE).getString(Constanttt.LOGIN_LONGITUDE, null);
-                        //A goi B id cua A,,,id A nam trong requestlist cua B
-
-                        database.child(Constanttt.USERS)
-                                .child(edtInputAddFr.getText().toString())
-                                .child(Constanttt.USERS_listWait)
-                                .child("FRIENDS").push().setValue(new Friend(myID, link, name, Double.parseDouble(x), Double.parseDouble(y)));
-                        ///////////////////////////
-                    }
-                });
-
+                guiLoiMoiKetBan();
                 break;
             case 1:
-                //danh sách lời mời
+                xemLoiMoiKetBan();
                 break;
             case 2:
                 //danh sách bạn bè
@@ -230,20 +143,77 @@ public class Fragment_ThongTin extends Fragment implements AdapterView.OnItemCli
                 break;
         }
     }
-    public void disconnectFromFacebook() {
 
-        if (AccessToken.getCurrentAccessToken() == null) {
-            return; // already logged out
-        }
+    private void xemLoiMoiKetBan() {
+        startActivity(new Intent(getContext(), ShowListAddFriend.class));
+    }
 
-        new GraphRequest(AccessToken.getCurrentAccessToken(), "/me/permissions/", null, HttpMethod.DELETE, new GraphRequest
-                .Callback() {
+    private void guiLoiMoiKetBan() {
+        final Dialog dialog0 = new Dialog(getContext());
+        dialog0.setTitle("Thêm bạn bè");
+        dialog0.setContentView(R.layout.dialog_add_friend);
+        dialog0.show();
+        //anh xa trong dialog add friend
+        edtInputAddFr = (EditText) dialog0.findViewById(R.id.edtInputAddFr);
+        Button btnXemThu, btnok, btncancel;
+        btncancel = (Button) dialog0.findViewById(R.id.btnCancelAddFr);
+        btnok = (Button) dialog0.findViewById(R.id.btnOkAddFr);
+        final TextView txttenXemThu = (TextView) dialog0.findViewById(R.id.txtXemThuName);
+        final ImageView imgADD = (ImageView) dialog0.findViewById(R.id.imgXemThu);
+        btnXemThu = (Button) dialog0.findViewById(R.id.btnXemAvata);
+
+
+        //btn xem thu avt nguoi ban
+        btnXemThu.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCompleted(GraphResponse graphResponse) {
+            public void onClick(View view) {
 
-                LoginManager.getInstance().logOut();
+                if (edtInputAddFr.getText().toString().equals("")) {
+                    Toast.makeText(getContext(), "Vui lòng nhập thông tin cần thiết để tìm người bạn!", Toast.LENGTH_SHORT).show();
+                } else {
+                           /* String a[] = getUserByID(edtInputAddFr.getText().toString());
+                            // Log.e(Constanttt.TAG_APP,"xxxx "+a[1]);
+                            txttenXemThu.setText(a[0]);
+                            Glide.with(getContext())
+                                    .load(a[1])
+                                    .into(imgADD);*/
+                    //load anh bang glide
+                    if (boolCLickXemTHu) {
+                        imgADD.setVisibility(View.INVISIBLE);
+
+                        boolCLickXemTHu = false;
+                    } else {
+                        imgADD.setVisibility(View.GONE);
+                        boolCLickXemTHu = true;
+                    }
+                }
+            }
+        });
+        //button cancel ko ket ban
+        btncancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog0.dismiss();
+            }
+        });
+        //button click gửi lời mới kết bạn
+        btnok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String myID = getContext()
+                        .getSharedPreferences(Constanttt.SHARE_REF_LOGIN, Context.MODE_PRIVATE).getString(Constanttt.LOGIN_ID, null);
+                String link = getContext()
+                        .getSharedPreferences(Constanttt.SHARE_REF_LOGIN, Context.MODE_PRIVATE).getString(Constanttt.LOGIN_LINK_IMG, null);
+                String name = getContext()
+                        .getSharedPreferences(Constanttt.SHARE_REF_LOGIN, Context.MODE_PRIVATE).getString(Constanttt.LOGIN_NAME, null);
+
+                //A goi B id cua A,,,id A nam trong requestlist cua B
+                database.child(Constanttt.USERS)
+                        .child(edtInputAddFr.getText().toString())
+                        .child(Constanttt.USERS_listWait)
+                        .child(myID).setValue(new Friend(myID, link, name));
 
             }
-        }).executeAsync();
+        });
     }
 }
